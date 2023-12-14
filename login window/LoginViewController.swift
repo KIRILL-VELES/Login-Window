e447ca6db2458e6b8aa472f07aaf82676fefb47f
Kirill Sinenchenko <votar@bk.ru>
2023-12-14 21:10:13 +0300

    Add logic

--- login window/LoginViewController.swift
+++ login window/LoginViewController.swift
@@ -19,22 +19,69 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         passwordTextField.delegate = self
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        
+        welcomeVC.nameTextField = nameTextField.text
+    }
+    
     // Скрыть клавиатуру по тапу на экране
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
         super.touchesBegan(touches, with: event)
     }
     
-    // Скрыть клавиатуру по нажатии Готво
+    // Скрыть клавиатуру по нажатии Готово
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
     
     @IBAction func loginButtonAction(_ sender: UIButton) {
-        print("CLICK")
+        guard let name = nameTextField.text, !name.isEmpty, name.rangeOfCharacter(from: .letters) != nil else {
+            showAlert(with: "Wrong!", message: "Please enter only letters in the name field")
+            return
+        }
+        
+        guard let password = passwordTextField.text, !password.isEmpty else {
+            showAlert(with: "Wrong!", message: "Please enter your password")
+            return
+        }
+        
     }
     
+    func showAlert(with title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if isBeingDismissed || isMovingFromParent {
+            showExitConfirmation()
+        }
+    }
+    
+    func showExitConfirmation() {
+        let alertController = UIAlertController(title: "Confirm Exit", message: "Do you want to log out and exit?", preferredStyle: .alert)
+        
+        let confirmAction = UIAlertAction(title: "Confirm", style: .destructive) { (_) in
+            self.navigationController?.popViewController(animated: true)
+            self.logout()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func logout() {
+        nameTextField.text = nil
+        passwordTextField.text = nil
+    }
 }
-
--- login window/WelcomeViewController.swift
+++ login window/WelcomeViewController.swift
@@ -9,20 +9,37 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
+    @IBOutlet weak var greetinglabel: UILabel!
+    
+    var nameTextField: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        greetinglabel.text = "Hello, \(nameTextField ?? "") !"
     }
-    
-
-    
-    
-    
-    
-    
     
     @IBAction func logOutButtonAction(_ sender: UIButton) {
-        print("Cclick")
+        showAlert(with: "Log Out", message: "Are you sure you want to log out?", style: .alert, actions: [
+            UIAlertAction(title: "Log Out", style: .destructive, handler: { (_) in
+                self.logOut()
+            }),
+            UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        ])
     }
     
+    func logOut() {
+        if let loginViewController = presentingViewController as? LoginViewController {
+            loginViewController.nameTextField.text = nil
+            loginViewController.passwordTextField.text = nil
+        }
+        dismiss(animated: true, completion: nil)
+    }
     
- }
+    func showAlert(with title: String, message: String, style: UIAlertController.Style, actions: [UIAlertAction]) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        for action in actions {
+            alertController.addAction(action)
+        }
+        present(alertController, animated: true, completion: nil)
+    }
+}
